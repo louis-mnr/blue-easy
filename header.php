70cbb74d2214304156559b863183d19ee869f70d
louis-mnr <meunierlouis21@hotmail.com>
2020-06-02 16:09:42 +0200

    commentaire

---
+++ header.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+
+
+  </head>
+  <body>
+
+<header>
+<div class="font-weight-bold">
+  <nav class="navbar navbar-expand-lg navopacity">
+      <div class="container">
+    <a class="navbar-brand" href="#">BLU<span class="text-info">E</span>ASY</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">HOME <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link active" href="#">SERVICES <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="#">FEATURES <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link active" href="#">PORTFOLIO <span class="sr-only">(current)</span></a>
+                                <a class="nav-item nav-link active" href="#">CONTACT <span class="sr-only">(current)</span></a>
+      </div>
+    </div>
+    </div>
+  </div>
+  </nav>
+  <div class="row">
+  <div class="col-12 textopacity margin-title"><center><h1><p>“I’M LOOKING FOR THE UNEXPECTED.</p>
+<p>I’M LOOKING FOR THINGS I’VE NEVER SEEN BEFORE.”</p></h1></div>
+  <div class="col-*-*"></div>
+</div>
+</center>
+</header>
+
+<section>
+  <div class="colservices">
+    <div class="container">
+  <div class="font-weight-light">
+<div class="col-lg-12"><h1>SERVICES</h1></div>
+  </div>
+    <div class="colservices2">
+<div class="row">
+  <div class="col-lg-3"><img src="logoservices1"><h1 class="font-weight-semibold">COFFEE</h1><p class="font-weight-bold">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p></div>
+  <div class="col-lg-3"><img src="logoservices2"><h1 class="font-weight-bold">INSTANT</h1><p class="font-weight-bold">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p></div>
+  <div class="col-lg-3"><img src="logoservices3"><h1 class="font-weight-bold">SERIOUS</h1><p class="font-weight-bold">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p></div>
+  <div class="col-lg-3"><img src="logoservices4"><h1 class="font-weight-bold">FRAME</h1><p class="font-weight-bold">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p></div>
+</div>
+</div>
+</div>
+</section>
+
+<section>
+<div class="font-weight-light">
+  <nav class="navbar navbar-expand-lg nav2">
+      <div class="container">
+    <div class="navbar-brand" href="#"><h1>PORTFOLIO</h1></div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="navbar-nav">
+        <div class="nav-item nav-link active" href="#"><p class="font-weight-bold">ALL </p><span class="sr-only">(current)</span></div>
+        <div class="nav-item nav-link active" href="#"><p class="font-weight-bold">PANORAMAS </p><span class="sr-only">(current)</span></div>
+                <div class="nav-item nav-link active" href="#"><p class="font-weight-bold">PORTRAITS </p><span class="sr-only">(current)</span></div>
+                        <div class="nav-item nav-link active" href="#"><p class="font-weight-bold">MACRO </p><span class="sr-only">(current)</span></div>
+                                <div class="nav-item nav-link active" href="#"><p class="font-weight-bold">JOURNAL </p><span class="sr-only">(current)</span></div>
+    </div>
+    </div>
+  </div>
+</div>
+  </nav>
+<div class="container">
+  <div class="row">
+    <div class="col"><img src="imgportfolio1"></div>
+    <div class="col"><img src="imgportfolio2"></div>
+    <div class="col"><img src="imgportfolio3"></div>
+    <div class="col"><img src="imgportfolio4"></div>
+  </div>
+  <div class="row">
+    <div class="col"><img src="imgportfolio5"></div>
+    <div class="col"><img src="imgportfolio6"></div>
+    <div class="col"><img src="imgportfolio7"></div>
+    <div class="col"><img src="imgportfolio8"></div>
+</div>
+</section>
+
+<section class="colvideo">
+    <div class="container">
+      <div class="row colvideo">
+  <div class="col-lg-6"><h1 class="font-weight-light">JUST DEFAULT SECTION</h1>
+    <p class="font-weight-bold">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+
+<p class="font-weight-bold">Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p></div>
+  <div class="col-lg-6"><iframe src="https://www.youtube.com/embed/tI1uTVaCGDE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</div>
+</div>
+</section>
